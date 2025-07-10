eaf0c7da03fc70c53e1a611c711f15e444c60be8
mr-dargi <dargimohammadreza@gmail.com>
2025-07-10 11:16:31 +0330

    Create Post model in blog app

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_at"]
+    
+    def __str__(self):
+        return self.title
