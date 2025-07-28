b2250fd24b41599ec2adc3716a3a575c35003cdb
mr-dargi <dargimohammadreza@gmail.com>
2025-07-28 19:44:45 +0330

    Regitster Post model in blog to admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+# Register Post in admin panel
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "slug",
+        "author",
+        "published",
+        "created_at",
+        "updated_at"
+    )
+
+    list_filter = (
+        'published',
+    )
+
+    search_fields = (
+        "title",
+        "author"
+    )
+
+    prepopulated_fields = {"slug": ("title", )}
+    ordering = ("-published", "created_at")
+
+
+admin.site.register(Post, PostAdmin)
