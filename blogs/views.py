576dea1db4332472c4eacfb358d56346f086c2e2
mr-dargi <dargimohammadreza@gmail.com>
2025-08-07 14:51:42 +0330

    Add pagination for posts

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, "blogs/home.html", {'posts': posts})
+    paginator = Paginator(posts, 4)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "blogs/home.html", {'posts': page_obj})
