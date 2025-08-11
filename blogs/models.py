1c29bd25ebbb8bdd47fd9e921dfc7d3548b31167
mr-dargi <dargimohammadreza@gmail.com>
2025-08-11 10:52:45 +0330

    Add commnets model in blog app models and add some changes to css for style of picture and post info to look better in mobile view

--- blogs/models.py
+++ blogs/models.py
@@ -18,3 +18,26 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies"
+    )
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created"]
+    
+
+    def __str__(self):
+        return f"کامنت از {self.user}"
+    
+
+    @property
+    def is_reply(self):
+        return self.parent is not None
