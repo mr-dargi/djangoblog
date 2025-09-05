7d90445bbc5dedac7fbc6fd788a667217cff0990
mr-dargi <dargimohammadreza@gmail.com>
2025-09-05 18:37:40 +0330

    feat(model): add Category model in blogs app

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 from accounts.models import User
+from django.template.defaultfilters import slugify
+
+
+class Category(models.Model):
+    parent = models.ForeignKey(
+        "self",
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="childs"
+    )
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
